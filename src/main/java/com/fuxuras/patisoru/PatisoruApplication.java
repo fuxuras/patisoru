1cf7ca08eb5994ffa5958406530e56deb9e15099
fuxuras <furkancanokal1@gmail.com>
2025-01-22 20:30:48 +0300

    Initial commit

---
+++ src/main/java/com/fuxuras/patisoru/PatisoruApplication.java
@@ -0,0 +1,13 @@
+package com.fuxuras.patisoru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatisoruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatisoruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fuxuras/patisoru/PatisoruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuxuras.patisoru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatisoruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
