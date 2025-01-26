152111b17149c2288cc1bef60be5d228d7d961a3
fuxuras <furkancanokal1@gmail.com>
2025-01-26 03:36:31 +0300

    fix: postgre doesent have binary16

--- src/main/java/com/fuxuras/patisoru/entities/abstracts/BaseEntity.java
+++ src/main/java/com/fuxuras/patisoru/entities/abstracts/BaseEntity.java
@@ -1,12 +1,10 @@
 package com.fuxuras.patisoru.entities.abstracts;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
@@ -19,8 +17,8 @@ import java.util.UUID;
 @Setter
 public abstract class BaseEntity implements Serializable {
     @Id
-    @GeneratedValue
-    @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
 
    @CreationTimestamp
