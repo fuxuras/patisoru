2a7019d09dd23f479b997a25697cc5a0e70c4395
fuxuras <furkancanokal1@gmail.com>
2025-01-26 03:36:45 +0300

    feat: userservice

---
+++ src/main/java/com/fuxuras/patisoru/services/UserService.java
@@ -0,0 +1,36 @@
+package com.fuxuras.patisoru.services;
+
+import com.fuxuras.patisoru.configuration.DtoMapper;
+import com.fuxuras.patisoru.dto.RegisterRequest;
+import com.fuxuras.patisoru.dto.ResponseMessage;
+import com.fuxuras.patisoru.entities.User;
+import com.fuxuras.patisoru.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public ResponseMessage createUser(RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail())){
+            ResponseMessage responseMessage = new ResponseMessage();
+            responseMessage.setMessage("Email already exists");
+            responseMessage.setCode(-1);
+            return responseMessage;
+        }
+        User user = DtoMapper.INSTANCE.RegisterRequestToUser(registerRequest);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        userRepository.save(user);
+        ResponseMessage responseMessage = new ResponseMessage();
+        responseMessage.setMessage("User created");
+        responseMessage.setCode(1);
+        return responseMessage;
+    }
+
+}
