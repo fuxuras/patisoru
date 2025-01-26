ea6bb4cb86db08ac85b776a28e732ecc4642e61a
fuxuras <furkancanokal1@gmail.com>
2025-01-26 03:34:56 +0300

    fix: table name conflict with postgres user table

--- src/main/java/com/fuxuras/patisoru/entities/User.java
+++ src/main/java/com/fuxuras/patisoru/entities/User.java
@@ -5,25 +5,28 @@ import com.fuxuras.patisoru.entities.abstracts.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User extends BaseEntity {
     private String email;
     private String password;
     private String fullName;
     private String role;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Comment> comments;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Post> posts;
 }
