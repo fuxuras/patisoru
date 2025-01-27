21dba9a7d9c2ba1b6e080da5e26aef1eeffc749b
fuxuras <furkancanokal1@gmail.com>
2025-01-27 04:07:43 +0300

    fix: mapper fix

--- src/main/java/com/fuxuras/patisoru/configuration/DtoMapper.java
+++ src/main/java/com/fuxuras/patisoru/configuration/DtoMapper.java
@@ -7,9 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface DtoMapper {
-    DtoMapper INSTANCE = Mappers.getMapper( DtoMapper.class );
-
     User RegisterRequestToUser(RegisterRequest registerRequest);
 }
