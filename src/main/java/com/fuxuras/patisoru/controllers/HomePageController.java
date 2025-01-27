807c0b7ba3d137abde7aae790815d6e6f00d6e26
fuxuras <furkancanokal1@gmail.com>
2025-01-28 02:53:04 +0300

    feat: featured posts

--- src/main/java/com/fuxuras/patisoru/controllers/HomePageController.java
+++ src/main/java/com/fuxuras/patisoru/controllers/HomePageController.java
@@ -1,12 +1,24 @@
 package com.fuxuras.patisoru.controllers;
 
+import com.fuxuras.patisoru.dto.FeaturedPost;
+import com.fuxuras.patisoru.services.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomePageController {
+
+    private final PostService postService;
+
     @GetMapping("/")
-    public String homepage(){
+    public String homepage(Model model) {
+        List<FeaturedPost> featuredPosts = postService.getFeaturedPosts();
+        model.addAttribute("featuredPosts", featuredPosts);
         return "homepage";
     }
 }
