4241df277818fef50136fa3b7aff1a96675468cc
fuxuras <furkancanokal1@gmail.com>
2025-01-28 02:52:07 +0300

    feat: featured posts

--- src/main/java/com/fuxuras/patisoru/repositories/PostRepository.java
+++ src/main/java/com/fuxuras/patisoru/repositories/PostRepository.java
@@ -3,7 +3,10 @@ package com.fuxuras.patisoru.repositories;
 import com.fuxuras.patisoru.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findTop5ByCreatedAtAfterOrderByLikeCountDesc(LocalDateTime dateTime);
 }
