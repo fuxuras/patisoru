2546fc4ee64cab6bbb50ebff4d7eb91964c36f11
fuxuras <furkancanokal1@gmail.com>
2025-01-28 02:53:01 +0300

    feat: featured posts

--- src/main/java/com/fuxuras/patisoru/services/PostService.java
+++ src/main/java/com/fuxuras/patisoru/services/PostService.java
@@ -1,13 +1,18 @@
 package com.fuxuras.patisoru.services;
 
 import com.fuxuras.patisoru.configuration.DtoMapper;
+import com.fuxuras.patisoru.dto.FeaturedPost;
 import com.fuxuras.patisoru.dto.PostResponse;
 import com.fuxuras.patisoru.entities.Post;
 import com.fuxuras.patisoru.repositories.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +25,13 @@ public class PostService {
         Post post = postRepository.findById(id).orElseThrow(()-> new RuntimeException("Post not found"));
         PostResponse postResponse = mapper.postToPostResponse(post);
         return postResponse;
+    }
+
+    public List<FeaturedPost> getFeaturedPosts(){
+        List<Post> posts = postRepository.findTop5ByCreatedAtAfterOrderByLikeCountDesc(LocalDateTime.now().minusMonths(1));
+        List<FeaturedPost> featuredPosts = posts.stream()
+                .map(mapper::postToFeaturedPost)
+                .collect(Collectors.toList());
+        return featuredPosts;
     }
 }
