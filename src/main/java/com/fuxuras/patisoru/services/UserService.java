d579cc6c6c39e641d22fed10ba468e119c471e79
fuxuras <furkancanokal1@gmail.com>
2025-01-29 07:16:41 +0300

    feat: find user by email

--- src/main/java/com/fuxuras/patisoru/services/UserService.java
+++ src/main/java/com/fuxuras/patisoru/services/UserService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -31,6 +33,10 @@ public class UserService {
         responseMessage.setMessage("Başarıyla kayıt oldunuz");
         responseMessage.setCode(1);
         return responseMessage;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
 }
