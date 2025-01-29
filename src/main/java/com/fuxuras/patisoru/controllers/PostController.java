f8fe2e1ec42712a39c0c0a88daa67c85397811b4
fuxuras <furkancanokal1@gmail.com>
2025-01-29 07:17:41 +0300

    feat: post create

--- src/main/java/com/fuxuras/patisoru/controllers/PostController.java
+++ src/main/java/com/fuxuras/patisoru/controllers/PostController.java
@@ -1,15 +1,16 @@
 package com.fuxuras.patisoru.controllers;
 
+import com.fuxuras.patisoru.dto.PostCreateRequest;
 import com.fuxuras.patisoru.dto.PostResponse;
-import com.fuxuras.patisoru.repositories.PostRepository;
+import com.fuxuras.patisoru.dto.ResponseMessage;
 import com.fuxuras.patisoru.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.UUID;
 
 @Controller
@@ -24,5 +25,20 @@ public class PostController {
         PostResponse post = postService.getPostById(id);
         model.addAttribute("post", post);
         return "/posts/single-post";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "/posts/post-form";
+    }
+    @PostMapping("/create")
+    public String create(@ModelAttribute PostCreateRequest post, Principal principal, RedirectAttributes redirectAttributes){
+        ResponseMessage message = postService.create(post, principal.getName());
+        redirectAttributes.addFlashAttribute("message", message);
+        if (message.getCode() > 0){
+            return "redirect:/";
+        }else {
+            return "redirect:/post/create";
+        }
     }
 }
