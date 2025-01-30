fb967e8ae815eee9d8a90a2fff9ad0187497376c
fuxuras <furkancanokal1@gmail.com>
2025-01-30 16:51:23 +0300

    feat: featured post id added

--- src/main/java/com/fuxuras/patisoru/dto/FeaturedPost.java
+++ src/main/java/com/fuxuras/patisoru/dto/FeaturedPost.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FeaturedPost {
+    private UUID id;
     private String title;
     private Long likeCount;
     private UserInFeaturedPost user;
