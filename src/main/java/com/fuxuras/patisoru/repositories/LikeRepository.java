5bd81531adc44a0e5e064256a3375e6d715edd46
fuxuras <furkancanokal1@gmail.com>
2025-02-05 19:10:18 +0300

    feat: like repository

---
+++ src/main/java/com/fuxuras/patisoru/repositories/LikeRepository.java
@@ -0,0 +1,19 @@
+package com.fuxuras.patisoru.repositories;
+
+import com.fuxuras.patisoru.entities.Like;
+import com.fuxuras.patisoru.entities.LikeId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
+
+public interface LikeRepository extends JpaRepository<Like, LikeId> {
+
+    @Query("SELECT COUNT(l) FROM Like l WHERE l.id.postId = :postId AND l.isLike = true")
+    long countLikesByPostId(@Param("postId") UUID postId);
+
+
+    @Query("SELECT COUNT(l) FROM Like l WHERE l.id.postId = :postId AND l.isLike = false")
+    long countDislikesByPostId(@Param("postId") UUID postId);
+}
