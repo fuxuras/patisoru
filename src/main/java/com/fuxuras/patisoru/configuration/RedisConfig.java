5e3da2c343eb1c8cbd208b7e22dd52d2f029c478
fuxuras <furkancanokal1@gmail.com>
2025-02-17 12:06:53 +0300

    fix: redis template added

--- src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
+++ src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -19,7 +21,6 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
-    @Primary
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10)) // Or configure via properties
@@ -30,5 +31,15 @@ public class RedisConfig {
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer()); // Or use GenericJackson2JsonRedisSerializer
+        template.afterPropertiesSet(); // VERY IMPORTANT
+        return template;
     }
 }
