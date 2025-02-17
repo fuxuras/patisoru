a889d15cbac44c060595566c509cae5b055d214b
fuxuras <furkancanokal1@gmail.com>
2025-02-17 16:42:32 +0300

    fix: we ball

--- src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
+++ src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
@@ -1,31 +1,28 @@
 package com.fuxuras.patisoru.configuration;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-import java.time.Duration;
-
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
+
+
+
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues()
-                .serializeValuesWith( RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisConfiguration =
+                new RedisStandaloneConfiguration();
+
+        redisConfiguration.setHostName("redis");
+        redisConfiguration.setPort(6379);
+        return new LettuceConnectionFactory(redisConfiguration);
+
     }
 }
