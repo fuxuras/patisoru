f9c4db3d6b923c185e450a9b397e5fffa5825198
fuxuras <furkancanokal1@gmail.com>
2025-02-17 17:43:43 +0300

    fix: serializable

--- src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
+++ src/main/java/com/fuxuras/patisoru/configuration/RedisConfig.java
@@ -3,9 +3,16 @@ package com.fuxuras.patisoru.configuration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @EnableCaching
 @Configuration
@@ -24,5 +31,18 @@ public class RedisConfig {
         redisConfiguration.setPort(6379);
         return new LettuceConnectionFactory(redisConfiguration);
 
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60)) // Set a default TTL (Time-To-Live) - adjust as needed
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())) // Serialize keys as Strings
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())) // Serialize values as JSON
+                .disableCachingNullValues(); // Don't cache null values
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
