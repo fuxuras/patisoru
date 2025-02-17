e36c39fb78d4c8e93d9cafc27d6e0b6493cf1fbf
fuxuras <furkancanokal1@gmail.com>
2025-02-17 16:53:07 +0300

    fix: serializable

--- src/main/java/com/fuxuras/patisoru/dto/FeaturedPost.java
+++ src/main/java/com/fuxuras/patisoru/dto/FeaturedPost.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class FeaturedPost {
+public class FeaturedPost implements Serializable {
     private UUID id;
     private String title;
     private Long likeCount;
