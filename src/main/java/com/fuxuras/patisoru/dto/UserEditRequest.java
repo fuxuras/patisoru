2b81dcf5436e05f090c6bb79873975d55587b4f4
fuxuras <furkancanokal1@gmail.com>
2025-04-12 19:34:30 +0300

    feat: profile validation

--- src/main/java/com/fuxuras/patisoru/dto/UserEditRequest.java
+++ src/main/java/com/fuxuras/patisoru/dto/UserEditRequest.java
@@ -1,5 +1,8 @@
 package com.fuxuras.patisoru.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -7,6 +10,11 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEditRequest {
+    @NotBlank(message = "Email alanı boş olamaz")
+    @Email(message = "Geçerli bir email formatı giriniz")
     private String email;
+    
+    @NotBlank(message = "İsim alanı boş olamaz")
+    @Size(min = 2, max = 100, message = "İsim 2-100 karakter arasında olmalıdır")
     private String fullName;
 }
