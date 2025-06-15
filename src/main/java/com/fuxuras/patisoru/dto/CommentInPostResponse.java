09edb9d5f0d0a5c6391a7a6344961520eafd325d
fuxuras <furkancanokal1@gmail.com>
2025-06-15 19:24:46 +0300

    feat: new single post design

--- src/main/java/com/fuxuras/patisoru/dto/CommentInPostResponse.java
+++ src/main/java/com/fuxuras/patisoru/dto/CommentInPostResponse.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
@@ -14,7 +15,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class CommentInPostResponse {
     private UUID id;
-    private Date createdAt;
+    private LocalDateTime createdAt;
     private String text;
     private UserInPostResponse user;
 }
