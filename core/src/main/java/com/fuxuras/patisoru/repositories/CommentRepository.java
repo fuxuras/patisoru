8e87df52ea2f8d715c19d9b31f80d931b8350d03
fuxuras <furkancanokal1@gmail.com>
2025-10-02 01:37:00 +0300

    feat(comment): Implement get comments by post and refactor create

--- core/src/main/java/com/fuxuras/patisoru/repositories/CommentRepository.java
+++ core/src/main/java/com/fuxuras/patisoru/repositories/CommentRepository.java
@@ -3,7 +3,9 @@ package com.fuxuras.patisoru.repositories;
 import com.fuxuras.patisoru.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findByPostId(UUID postId);
 }
